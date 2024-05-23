63f2ba70460f4bd149680cde3a00094d81a326c9
NabeelaAkram <163669804+NabeelaAkram@users.noreply.github.com>
2024-05-23 16:56:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,35 @@
+
+import { useState } from "react";
+import axios from "axios"
+import { Navigate, useNavigate } from "react-router-dom";
+
+function App() {
+  const navigate=useNavigate()
+const [user,setuser]=useState()
+const [pass,setpass]=useState()
+function handleuser(evt){
+  setuser(evt.target.value)
+}
+function handlepass(evt){
+  setpass(evt.target.value)
+}
+function check(){
+  var logindetails=axios.post("http://localhost:5001/login",{"username":user,"password":pass})
+  logindetails.then(function(data){
+    if(data.data==true){
+      console.log(data)
+      navigate("/success")}
+      else{
+        navigate("/fail")
+      }
+    
+  })
+}
+  return (<div>
+    <input placeholder="username" name="username" onChange={handleuser}></input>
+    <input placeholder="password" name="password" onChange={handlepass}></input>
+    <button onClick={check}>Login</button></div>
+  )
+}
+
+export default App;
